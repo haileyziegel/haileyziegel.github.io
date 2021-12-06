7d65a4709ff2a9a5a1285b1e05a0dad8a8086e05
haileyziegel <zerohz2019@gmail.com>
2021-12-06 15:26:27 +0000

    describe your changes here

--- projects/platformer/js/init/cannon.js
+++ projects/platformer/js/init/cannon.js
@@ -27,8 +27,8 @@
         // ALL YOUR CODE GOES BELOW HERE ///////////////////////////////////////
         
         // example: 
-        createCannon("top", 450);
-        createCannon("right", 210);
+        createCannon("bottom", 450);
+        createCannon("bottom", 210);
         createCannon("bottom", 209);
         
         // ALL YOUR CODE GOES ABOVE HERE ///////////////////////////////////////
